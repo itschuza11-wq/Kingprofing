3968919d1934200bea607cb3ffe85b105075810e
itschuza11-wq <itschuza11@gmail.com>
2025-11-06 21:45:31 +0500

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://YOUR-PROJECT.supabase.co";
-const supabaseAnonKey = "YOUR-ANON-KEY";
+const supabaseUrl = "https://wrerbktgtfawvndmtzca.supabase.co";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndyZXJia3RndGZhd3ZuZG10emNhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE5Mjk4ODksImV4cCI6MjA3NzUwNTg4OX0.oFGY4NIpbVFrkRrSwXyLCEMfIgMmS1kE-kbbEFBXa6Q";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
